c44cb6d96cb0139914164b9012fbe08465db13b5
Christophe Chabanois <christophe.chabanois@soat.fr>
2012-10-30 15:09:35 +0100

    Changed from org.soat to fr.soat

--- socialnetwork-parent/socialnetwork-ui/src/main/java/fr/soat/socialnetwork/ui/HelloWorldController.java
+++ socialnetwork-parent/socialnetwork-ui/src/main/java/fr/soat/socialnetwork/ui/HelloWorldController.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.socialnetwork.ui;
+package fr.soat.socialnetwork.ui;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
