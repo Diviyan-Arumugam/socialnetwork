5bffdce7434de9a2e7c5c89d6b2ac4aa7712b7c9
Christophe Chabanois <christophe.chabanois@soat.fr>
2012-11-15 17:24:05 +0100

    Pass first test !!!

--- socialnetwork-parent/socialnetwork-dao/src/main/java/fr/soat/socialnetwork/DataBaseProducer.java
+++ socialnetwork-parent/socialnetwork-dao/src/main/java/fr/soat/socialnetwork/DataBaseProducer.java
@@ -9,7 +9,7 @@ import javax.persistence.PersistenceContext;
 
 public class DataBaseProducer
 {
-    @PersistenceContext(unitName="default")
+    @PersistenceContext(unitName="soatsocial")
     private EntityManager entityManager;
 
     @Produces
